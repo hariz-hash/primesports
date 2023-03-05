caad0c8a38858f5f37c4c807a8d399cb5f55c706
hariz <hariznyp@gmail.com>
2023-03-05 19:48:42 +0000

    cart done

--- routes/cart.js
+++ routes/cart.js
@@ -32,5 +32,26 @@ router.get('/:shoe_id/add', checkIfAuthenticated, async function (req, res) {
     res.redirect('/carts')
 })
 
+router.get('/:shoe_id/remove', checkIfAuthenticated, async (req,res)=> {
+    const userId = req.session.user.id;
+    const shoe_id = req.params.shoe_id;
+
+    await deleteFromCart(userId, shoe_id)
+
+    req.flash("success_messages", 'Item has been removed');
+    res.redirect('/carts');
+})
+
+router.post('/:shoe_id/update', checkIfAuthenticated, async(req,res)=>{
+    const userId = req.session.user.id;
+    const updateQuantity = req.body.newQty;
+    const shoe_id = req.params.shoe_id;
+
+    await setQuantity(userId, shoe_id, updateQuantity);
+    req.flash('success_messages', "The quantity has been updated");
+    res.redirect('/carts');
+  })
+
+
 
 module.exports = router; // #3 export out the router
